7bf6448946b67a0af7360dcfead91e89e218c12d
M3roof <43769947+M3roof@users.noreply.github.com>
2019-02-25 15:47:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,32 +9,38 @@ client.on('ready', () => {
 
 
 
-const developers = ["476335874343305221"]
-const adminprefix = "+";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
+
+
+client.on('ready', async() => {
+var server = "516364082127831040"; // ايدي السررفر
+var channel = "541717138142920745";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('$fa')
+    client.guilds.get(server).channels.get(channel).send('ان الله يحب المؤمنين')
+    },305);
+})
+
+
+
+
+    client.on('message', message => {
+        var prefix = "2";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
       
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wat')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Status You  ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
-      message.channel.send(`**Status You ${argresult} **`)
-}
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
 });
-
-
-
 
 
 
